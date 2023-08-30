d001c1bf33ab0f69522ba3e74ccb2097b8671acb
Dulce Ulgar <Dulce@DulceUlgar.com>
2023-08-30 10:28:10 +0200

    making folders of backend and create ad.controller

---
+++ skill-swap-frontend/skill-swap-backend/controllers/ad.controller.js
@@ -0,0 +1,8 @@
+exports.getAllAds = (req, res) => {
+    // Lógica para obtener todos los anuncios
+  };
+  
+  exports.createAd = (req, res) => {
+    // Lógica para crear un nuevo anuncio
+  };
+  
