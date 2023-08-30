bc0d5c54a1c340c8c2a1d06482efb63aef43707d
Dulce Ulgar <Dulce@DulceUlgar.com>
2023-08-30 09:10:00 +0200

    making the ts of profile

--- skill-swap-frontend/src/app/profile/profile.component.ts
+++ skill-swap-frontend/src/app/profile/profile.component.ts
@@ -1,10 +1,10 @@
+// profile.component.ts
 import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  // ...
 })
 export class ProfileComponent {
-
+  // Lógica para obtener y mostrar los datos del perfil del usuario
 }
