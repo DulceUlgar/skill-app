38f93aa5c3367f568055bf9ccb935946bd7f771e
Dulce Ulgar <Dulce@DulceUlgar.com>
2023-08-30 09:06:37 +0200

    making the ts og login

--- skill-swap-frontend/src/app/login/login.component.ts
+++ skill-swap-frontend/src/app/login/login.component.ts
@@ -1,10 +1,10 @@
+// login.component.ts
 import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  // ...
 })
 export class LoginComponent {
-
+  // Lógica del formulario de inicio de sesión
 }
