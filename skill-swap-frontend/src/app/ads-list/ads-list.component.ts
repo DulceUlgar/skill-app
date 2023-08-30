08913274abbd10526f4b75afd7edc67c9eda404a
Dulce Ulgar <Dulce@DulceUlgar.com>
2023-08-30 09:11:01 +0200

    making the ts of ads-list

--- skill-swap-frontend/src/app/ads-list/ads-list.component.ts
+++ skill-swap-frontend/src/app/ads-list/ads-list.component.ts
@@ -1,10 +1,10 @@
+// ads-list.component.ts
 import { Component } from '@angular/core';
+import { AdService } from '../ad.service';
 
 @Component({
-  selector: 'app-ads-list',
-  templateUrl: './ads-list.component.html',
-  styleUrls: ['./ads-list.component.scss']
+  // ...
 })
 export class AdsListComponent {
-
+  // Lógica para obtener y mostrar la lista de anuncios
 }
