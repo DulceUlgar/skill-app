e88ccf74ea72a64129331cac0e67b2b67da48c54
Dulce Ulgar <Dulce@DulceUlgar.com>
2023-08-30 09:12:26 +0200

    making the ts of ad-create

--- skill-swap-frontend/src/app/ad-create/ad-create.component.ts
+++ skill-swap-frontend/src/app/ad-create/ad-create.component.ts
@@ -1,10 +1,10 @@
+// ad-create.component.ts
 import { Component } from '@angular/core';
+import { AdService } from '../ad.service';
 
 @Component({
-  selector: 'app-ad-create',
-  templateUrl: './ad-create.component.html',
-  styleUrls: ['./ad-create.component.scss']
+  // ...
 })
 export class AdCreateComponent {
-
+  // Lógica del formulario para crear anuncios
 }
